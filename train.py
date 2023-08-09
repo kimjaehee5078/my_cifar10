430b09551b9225f8e1464271b53eed77a977089a
Ubuntu <jackie5078@naver.com>
2023-08-09 08:17:49 +0000

    first day

--- train.py
+++ train.py
@@ -3,6 +3,9 @@ import torchvision
 import os
 import torch
 from models.vgg import vgg11_bn
+import torch.nn as nn
+import torch.optim as optim
+import time
 
 
 def train(opt):
@@ -21,23 +24,45 @@ def train(opt):
 
     # Network model
     model = vgg11_bn()
-    pass # debug checkpoint
 
+    # GPU-support
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    
+    # Loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    start_epoch, end_epoch = (0, epochs)
+    for epoch in range(start_epoch, end_epoch):
+        print('epoch: %d/%d' % (epoch, end_epoch-1))
+        t0 = time.time()
+        epoch_loss = train_one_epoch(train_dataloader, model, loss_fn, optimizer, device)
+        t1 = time.time()
+        print('loss=%.4f (took %.2f sec)/n' % (epoch_loss, t1-t0))
+
+
+def train_one_epoch(train_dataloader, model, loss_fn, optimizer, device):
+    model.train()
+    losses =[]
     for i, (imgs, targets) in enumerate(train_dataloader):
-        print('batch idx=%d/%d' %(i, len(train_dataloader)-1))
-        pass # debug checkpoint
-
-    pass # debug breakpoint
-
-
-    print(epochs)
-    print(batch_size)
-    print(name)
+        imgs, targets = imgs.to(device), targets.to(device)
+        optimizer.zero_grad() # zeros the parameter gradients
+        preds = model(imgs)  # forward
+        loss = loss_fn(preds, targets) # calculates the iteration loss
+        loss.backward()       # backwad
+        optimizer.step()      # update weights
+        # print the iteration loss every 100 iterations
+        if i % 100 == 0:
+            print('\t iteration: %d/%d, loee=%.4f' % (i, len(train_dataloader)-1, loss))
+        losses.append(loss.item())
+    return torch.tensor(losses).mean().item()
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=200, help='target epochs')
-    parser.add_argument('--batch-size', type=int, default=200, help='batch size')
+    parser.add_argument('--batch-size', type=int, default=512, help='batch size')
     parser.add_argument('--name', default='ohhan', help='name for the run')
 
     opt = parser.parse_args()
