a2ae8dba89a02518f57081db693e96d346826665
Ubuntu <jackie5078@naver.com>
2023-08-09 07:34:37 +0000

    added a model

--- train.py
+++ train.py
@@ -1,9 +1,35 @@
 import argparse
+import torchvision
+import os
+import torch
+from models.vgg import vgg11_bn
+
 
 def train(opt):
     epochs = opt.epochs
     batch_size = opt.batch_size
     name = opt.name
+
+    
+    # Trian dataset
+    transforms = torchvision.transforms.ToTensor()
+    train_dataset = torchvision.datasets.CIFAR10('./data', train=True, download=True, transform=transforms)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'forg', 'horse', 'ship', 'truck')
+    num_workers = min([os.cpu_count(), batch_size])
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+
+    # Network model
+    model = vgg11_bn()
+    pass # debug checkpoint
+
+    for i, (imgs, targets) in enumerate(train_dataloader):
+        print('batch idx=%d/%d' %(i, len(train_dataloader)-1))
+        pass # debug checkpoint
+
+    pass # debug breakpoint
+
+
     print(epochs)
     print(batch_size)
     print(name)
@@ -17,3 +43,4 @@ if __name__ == '__main__':
     opt = parser.parse_args()
 
     train(opt)
+
